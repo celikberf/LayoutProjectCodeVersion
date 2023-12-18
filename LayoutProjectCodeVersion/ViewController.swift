99c157e011b64a68cc51acd6a293013fc6c4167b
Berfin Çelik <119796598+celikberf@users.noreply.github.com>
2023-12-18 13:32:50 +0100

    Initial Commit

---
+++ LayoutProjectCodeVersion/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LayoutProjectCodeVersion
+//
+//  Created by Berfin on 18.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
